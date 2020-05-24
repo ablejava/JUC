b101ea7dd978302d00b97566261cdc4a80772b25
ablejava <1298243794@qq.com>
2020-05-24 10:36:34 +0800

    多线程与高并发

--- src/main/java/com/juc/thread/volatiolTest/MyThread.java
+++ src/main/java/com/juc/thread/volatiolTest/MyThread.java
@@ -8,7 +8,11 @@ public class MyThread extends Thread {
 
     // volatile主要使用的场合是多个线程总可以感知实例变量被更改了，并且可以获取最新的值的使用，也就是用多线程读取共享变量可以获取最新值的使用
     // 是count ++ 并不是原子性操作 所以所 volatile本身并不处理数据的原子性 可以使用AtomicInteger原子类来实现
-    private static void addCount() {
+
+    /**
+     * 解决：方法上加synchronized
+     */
+    private /*synchronized*/ void addCount() {
         for (int i =0; i<100; i++) {
             count ++;
         }
